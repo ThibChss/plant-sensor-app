8ae72b6d2ac62031376421cbc03d034b3e8d988e
Thibault C. <thibault.chassine@gmail.com>
2026-04-03 11:55:04 +0200

    Add annotate gem

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: plants
+#
+#  id                                                                                          :uuid             not null, primary key
+#  growth_data(Additional growth data)                                                         :jsonb
+#  ideal_humidity(Ideal humidity on a scale of 1 to 10, 1 being very dry, 10 being very humid) :integer
+#  image_url                                                                                   :string
+#  max_temp(Maximum temperature in Celsius)                                                    :float
+#  min_temp(Minimum temperature in Celsius)                                                    :float
+#  name                                                                                        :string
+#  scientific_name                                                                             :string
+#  translated_name                                                                             :jsonb
+#  created_at                                                                                  :datetime         not null
+#  updated_at                                                                                  :datetime         not null
+#  trefle_id                                                                                   :string
+#
+# Indexes
+#
+#  index_plants_on_trefle_id  (trefle_id) UNIQUE
+#
 class Plant < ApplicationRecord
   ACCESSORS_KEYS = %i[light
                       sowing
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :uuid             not null, primary key
+#  email_address   :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email_address  (email_address) UNIQUE
+#
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
