975b0729513eccdefe21e293d65985fe4846f001
Thibault C. <thibault.chassine@gmail.com>
2026-04-03 13:36:35 +0200

    Setup PWA

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
+  # Force SSL for all requests.
+  config.force_ssl = true
+
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   mount RailsIcons::Engine, at: '/rails_icons'
 
+  get 'manifest.json', to: 'rails/pwa#manifest', as: :pwa_manifest, defaults: { format: :json }
+  get 'service-worker.js', to: 'rails/pwa#service_worker', as: :pwa_service_worker,
+                              defaults: { format: :js }
+
   resource :session, only: %i[new create destroy]
   resources :passwords, only: %i[new create edit update], param: :token
   resources :registrations, only: %i[new create]
