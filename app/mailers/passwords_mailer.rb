7e77445628e82941a547b494e5e92b0355445827
Thibault C. <thibault.chassine@gmail.com>
2026-04-10 12:37:24 +0200

    Minor changes

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -2,6 +2,8 @@ class PasswordsMailer < ApplicationMailer
   def reset(user)
     @user = user
 
-    mail subject: "Reset your password", to: user.email_address
+    I18n.with_locale(@user.locale.presence || I18n.default_locale) do
+      mail subject: I18n.t('passwords_mailer.reset.subject'), to: user.email_address
+    end
   end
 end
