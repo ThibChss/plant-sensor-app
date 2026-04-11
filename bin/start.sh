92d9299dcef8a43efb233181632a55669d14d875
Thibault C. <thibault.chassine@gmail.com>
2026-04-11 18:43:53 +0200

    Add start.sh

---
+++ bin/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+bundle exec rails runner "load Rails.root.join('db/queue_schema.rb') unless ActiveRecord::Base.connection.table_exists?('solid_queue_jobs')"
+bundle exec rails runner "load Rails.root.join('db/cable_schema.rb') unless ActiveRecord::Base.connection.table_exists?('solid_cable_messages')"
+bundle exec rake db:migrate
+bundle exec bin/rails server -b 0.0.0.0 -p ${PORT:-3000} -e $RAILS_ENV
