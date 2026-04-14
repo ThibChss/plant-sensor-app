0c714a363d72e8cf315dfdb9afb5e01fe8c6740f
Thibault C. <thibault.chassine@gmail.com>
2026-04-14 15:57:42 +0200

    Add start solis_queue to start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -4,4 +4,5 @@ set -e
 bundle exec rails runner "load Rails.root.join('db/queue_schema.rb') unless ActiveRecord::Base.connection.table_exists?('solid_queue_jobs')"
 bundle exec rails runner "load Rails.root.join('db/cable_schema.rb') unless ActiveRecord::Base.connection.table_exists?('solid_cable_messages')"
 bundle exec rake db:migrate
+bundle exec bin/rails solid_queue:start &
 bundle exec bin/rails server -b 0.0.0.0 -p ${PORT:-3000} -e $RAILS_ENV
